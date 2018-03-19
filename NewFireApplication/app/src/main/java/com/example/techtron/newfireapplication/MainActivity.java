7ba9951f9d507310ca265d806aa389c42cd919a5
Phillip <phillipables1@gmail.com>
2018-03-19 18:35:43 -0400

    instance of firebase database and myRef as getReference and setValue as a test case

--- NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/MainActivity.java
+++ NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.techtron.newfireapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Retrieve an instance of your database using getInstance()
+        // and reference the location you want to write to.
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("MyMessage");
+
+        myRef.setValue("Hello World");
     }
 }
