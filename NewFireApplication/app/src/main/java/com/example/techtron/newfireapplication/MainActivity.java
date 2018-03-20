6bf1be89a74d112611db681de079fa1ae76d238f
Phillip <phillipables1@gmail.com>
2018-03-20 08:15:07 -0400

    moved our database and reference instance back into on create method and added a declaration of them in the main class

--- NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/MainActivity.java
+++ NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/MainActivity.java
@@ -3,25 +3,31 @@ package com.example.techtron.newfireapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
 
-    public void sendMessage(View view){
         //Retrieve an instance of your database using getInstance()
         // and reference the location you want to write to.
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("MyMessage");
+    }
 
-        myRef.setValue("Hello World");
+    public void sendMessage(View view){
+        EditText editTextMessage = (EditText) findViewById(R.id.editTextMessage);
+        String message = editTextMessage.getText().toString();
+
+        myRef.setValue(message);
     }
 }
