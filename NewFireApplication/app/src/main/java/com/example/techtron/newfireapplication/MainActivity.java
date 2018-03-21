12e24232adf4fdec62d5df388fc606d81aa99c08
Phillip <phillipables1@gmail.com>
2018-03-21 15:18:08 -0400

    remove everything from activity main java

--- NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/MainActivity.java
+++ NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,65 +22,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
-
-    private ListView listView;
-    private ArrayList<String> mUserName = new ArrayList<>();
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Users");
-
-        listView = (ListView) findViewById(R.id.listView);
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mUserName);
-        listView.setAdapter(arrayAdapter);
-
-        myRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String value = dataSnapshot.getValue(String.class);
-                mUserName.add(value);
-                arrayAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                mUserName.remove(value);
-                arrayAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
 
-
-
-    public void sendMessage(View view){
-//        EditText editTextName = (EditText) findViewById(R.id.editTextName);
-//        EditText editTextContact = (EditText) findViewById(R.id.editTextContact);
-//        myRef = database.getReference();
-//
-//        myRef.child("Name").setValue(editTextName.getText().toString());
-//        myRef.child("Contact").setValue(editTextContact.getText().toString());
-    }
 }
