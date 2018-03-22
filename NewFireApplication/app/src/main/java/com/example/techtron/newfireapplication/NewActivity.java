ebb85ee3dbea9855e43c8871b0d1f624ee17d343
Phillip <phillipables1@gmail.com>
2018-03-22 16:03:07 -0400

    override on activity result, if request code and result code check out than uri is data.getData, storage reference is filepath child photoes child uri.lastpath, filepath put file uri add success listener

--- NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
+++ NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
@@ -1,13 +1,16 @@
 package com.example.techtron.newfireapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class NewActivity extends AppCompatActivity {
 
@@ -36,5 +39,19 @@ public class NewActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, GALLERY_INTENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            StorageReference filepath = storageReference.child("Photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    
+                }
+            })
+        }
     }
 }
