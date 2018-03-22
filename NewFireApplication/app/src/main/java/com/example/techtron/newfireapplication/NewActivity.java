52f97a456605b883fe823476f1c62d5e845d4df2
Phillip <phillipables1@gmail.com>
2018-03-22 15:23:27 -0400

    fixed layout and declared firebase auth and auth listener in new activity

--- NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
+++ NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
@@ -3,7 +3,13 @@ package com.example.techtron.newfireapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class NewActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
