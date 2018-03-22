971c5aa1c467e08e99551237aa48d6b4a7edaa1e
Phillip <phillipables1@gmail.com>
2018-03-22 15:29:27 -0400

    singout Method instantiates a instance of firebase auth which we then call the firebase auth signout method through

--- NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
+++ NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
@@ -2,6 +2,7 @@ package com.example.techtron.newfireapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -10,10 +11,14 @@ public class NewActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+    }
+
+    public void signOut(View v){
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
     }
 }
