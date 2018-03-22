c6cb9049fc1a3ab7df6da052968105e9205c8738
Phillip <phillipables1@gmail.com>
2018-03-22 15:52:57 -0400

    declare storage reference onCreate getReference of our getInstance of firebase storage

--- NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
+++ NewFireApplication/app/src/main/java/com/example/techtron/newfireapplication/NewActivity.java
@@ -6,16 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class NewActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
 
+    private StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
     }
 
     public void signOut(View v){
@@ -23,5 +29,9 @@ public class NewActivity extends AppCompatActivity {
         mAuth.signOut();
 
         startActivity(new Intent(NewActivity.this, MainActivity.class));
+    }
+
+    void imageUploadClicked(){
+
     }
 }
